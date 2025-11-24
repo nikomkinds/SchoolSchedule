2fc590035250fb6ea67b6cb2cf000cc4993bd9b0
DmitriyNikomkin <134718130+str1xoid@users.noreply.github.com>
2025-11-24 14:03:43 +0300

    feat: LoadConfig in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"github.com/nikomkinds/SchoolSchedule/internal/config"
+	"log/slog"
+)
+
+func main() {
+
+	// Loading config (connection params)
+	_, err := config.LoadConfig() // cfg, err !!!
+	if err != nil {
+		slog.Error("Failed to load config:", err)
+	}
+	slog.Info("Load config")
+}
